ce2b8d2115de81635282cd4bc18bd5cc76e962d1
janae henry <henry_j@qwasar.io>
2021-10-09 16:54:06 +0000

    please work, fingers crossed

---
+++ ex00/my_string_index.js
@@ -0,0 +1,15 @@
+
+ function my_string_index(param_1, param_2) {
+     for (let i = 0; i < param_1.length ; i++) {
+        if (param_1[i] == param_2)        
+        return i
+       
+       }
+ return -1
+ };
+
+/*function my_string_index(param_1, param_2) {
+ return param_1.indexOf(param_2);
+};
+
+console.log(my_string_index("hola", "l"));*/
